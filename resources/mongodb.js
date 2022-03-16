3641adc87a5373064111d2e2c57bdf57ab1d70cf
Muhammad Sameer <sameeramin1997@gmail.com>
2022-03-16 19:50:01 +0500

    Updated mongodb URI

--- resources/mongodb.js
+++ resources/mongodb.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 
-var URI = process.env.MONGODB_URI;
+var URI =
+    process.env.MONGODB_URI || "mongodb://localhost:27017/web-health-check";
 
 // Read all urls
 async function dbReadAll() {
